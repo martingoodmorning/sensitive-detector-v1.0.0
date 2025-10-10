4ae5eff8e0ac863bbf04c08eb75cf9c1510881d8
saisai5203 <16118803+saisai5203@user.noreply.gitee.com>
2025-10-10 17:09:28 +0800

    修改错误

--- scripts/gitee-deploy.sh
+++ scripts/gitee-deploy.sh
@@ -28,7 +28,7 @@ log_step() {
 
 # 配置变量
 GITEE_USERNAME="saisai5203"
-REPO_NAME="sensitive-detector"
+REPO_NAME="sensitive-detector-v1.0.0"
 VERSION="1.0.0"
 PACKAGE_NAME="sensitive-detector-v${VERSION}"
 GITEE_URL="https://gitee.com/${GITEE_USERNAME}/${REPO_NAME}"
@@ -105,7 +105,7 @@ EOF
 download_package() {
     log_step "下载部署包..."
     
-    PACKAGE_URL="${GITEE_URL}/releases/${PACKAGE_NAME}.tar.gz"
+    PACKAGE_URL="${GITEE_URL}/raw/master/releases/${PACKAGE_NAME}.tar.gz"
     
     log_info "下载地址: ${PACKAGE_URL}"
     
